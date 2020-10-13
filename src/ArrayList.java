1a75897e1a43da18295eee84fd84f7f84c307760
Addison Hoff <addisonhoff@addisons-air.kentdenver.org>
2020-10-13 11:40:35 -0600

    Created get method

--- src/ArrayList.java
+++ src/ArrayList.java
@@ -1,3 +1,4 @@
+import java.lang.IndexOutOfBoundsException;
 public class ArrayList<T>{
 
     private T obj;
@@ -12,7 +13,19 @@ public class ArrayList<T>{
 
     public T remove(int i) {}
 
-    public T get(int i) {}
+    public Object get(int i) {
+        //Check whether i is in bounds
+        if (i > length) {
+            throw new ArrayIndexOutOfBoundsException("Requested index out of bounds.");
+        } else {
+            //Loop through using .getChild to get target node
+            Nodes tempNode = firstNode;
+            for (int x = 0; x < i; x++) {
+                firstNode.getChild();
+            }
+            return tempNode.getObj();
+        }
+    }
 
     public T set(int i, T data) {}
 
